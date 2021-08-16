4b9c7cf86332fae02d46568ca80d83a6cd7fe76e
Abdul-Moize <abdul.moize@arbisoft.com>
2021-08-16 16:59:38 +0500

    fix black errors

--- weatherman.py
+++ weatherman.py
@@ -13,19 +13,21 @@ def main():
     :return:
     """
     parameters = sys.argv[1:]
-    accepted_flags = ['-e']
-    accepted_regex = [r'\d{4}']
+    accepted_flags = ["-e"]
+    accepted_regex = [r"\d{4}"]
     flag_handler = [t1]
     i = 0
-    path = ''
+    path = ""
     while i < len(parameters):
-        path = 'weatherfiles/'
+        path = "weatherfiles/"
         if len(parameters) < 2:
-            parameters = input('Too less options.'
-                               ' make sure command is like: "path flag date" path is optional')
+            parameters = input(
+                "Too less options."
+                ' make sure command is like: "path flag date" path is optional'
+            )
             i = 0
             continue
-        if parameters[0][0] != '-' and len(parameters) >= 3:
+        if parameters[0][0] != "-" and len(parameters) >= 3:
             path, parameters = parameters[0], parameters[1:]
         valid_flag = False
         handler = flag_handler[0]
@@ -36,19 +38,21 @@ def main():
                 valid_flag = True
                 break
         if not valid_flag:
-            parameters = input(f'Invalid Flag \'{parameters[i]}\'. '
-                               f'Please enter a valid command: ').split(' ')
+            parameters = input(
+                f"Invalid Flag '{parameters[i]}'. " f"Please enter a valid command: "
+            ).split(" ")
             i = 0
             continue
-        if re.match(accepted_regex[j], parameters[i+1]):
-            handler(parameters[i+1], path)
+        if re.match(accepted_regex[j], parameters[i + 1]):
+            handler(parameters[i + 1], path)
         else:
-            parameters = input(f'Invalid date \'{parameters[i+1]}\'. '
-                               f'Please enter a valid command: ').split(' ')
+            parameters = input(
+                f"Invalid date '{parameters[i+1]}'. " f"Please enter a valid command: "
+            ).split(" ")
             i = 0
             continue
         i += 2
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
