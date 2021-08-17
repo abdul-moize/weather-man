5d184c14176480501762bfa5f8a701c0b59b4a3d
Abdul-Moize <abdul.moize@arbisoft.com>
2021-08-17 16:42:56 +0500

    resolve conflict

--- weatherman.py
+++ weatherman.py
@@ -1,61 +1,73 @@
 """
 weatherman is a software that generates reports about the past weather of murree
 """
+import getopt
 import re
 import sys
 
-from modules.average_month import get_averages_month
+import constants
 from modules.extreme_year import extreme_temperatures_year
+from modules.average_month import get_averages_month
 
 
 def main():
     """
-    this is a driver function for weatherman.
-    :return: nothing
+    The driver function for weatherman.
+    Returns:
+        None
     """
-    parameters = sys.argv[1:]
+    parameters, args = getopt.getopt(sys.argv[1:], ":e:a:")
+    if args:
+        path, parameters = args[0], getopt.getopt(args[1:], ":e:a:")[0]
+    iteration = 0
     accepted_flags = ["-e", "-a"]
-    accepted_regex = [r"\d{4}", r"\d{4}/(0[1-9]|1[012]|[1-9])"]
+    accepted_regex = [r"\d{4}", r"\d{4}/\d{1,2}"]
     flag_handler = [extreme_temperatures_year, get_averages_month]
-    i = 0
     path = ""
-    while i < len(parameters):
-        if path == "":
-            path = "weatherfiles/"
-            if len(parameters) < 2:
-                parameters = input(
-                    "Too less options."
-                    ' make sure command is like: "path flag date" path is optional'
-                )
-                i = 0
-                continue
-            if parameters[0][0] != "-" and len(parameters) >= 3:
-                path, parameters = parameters[0], parameters[1:]
+    while iteration < len(parameters):
+        if len(parameters) < 1:
+            parameters = input(
+                "Invalid command. "
+                "The correct command format is: 'weatherman.py path flag date' "
+                "path is optional"
+            )
+            iteration = 0
+            path = constants.WEATHER_FILES_DIR
+            parameters, args = getopt.getopt(parameters, ":e:a:")
+            if args:
+                path, parameters = args[0], getopt.getopt(args[1:], ":e:a:")[0]
+            continue
         valid_flag = False
         handler = flag_handler[0]
         j = 0
-        for j, val in enumerate(accepted_flags):
-            if parameters[i] == val:
-                handler = flag_handler[j]
-                valid_flag = True
-                break
+        if accepted_flags.__contains__(parameters[iteration][0]):
+            handler = flag_handler[accepted_flags.index(parameters[iteration][0])]
+            valid_flag = True
         if not valid_flag:
             parameters = input(
-                f"Invalid Flag '{parameters[i]}'. " f"Please enter a valid command: "
+                f"Invalid Flag '{parameters[iteration][0]}'. "
+                f"Please enter a valid command: "
             ).split(" ")
-            i = 0
-            path = ""
+            parameters, args = getopt.getopt(parameters, ":e:a:")
+            path = constants.WEATHER_FILES_DIR
+            if args:
+                path, parameters = args[0], getopt.getopt(args[1:], ":e:a:")[0]
+            iteration = 0
             continue
-        if re.match(accepted_regex[j], parameters[i + 1]):
-            handler(parameters[i + 1], path)
+        if re.match(accepted_regex[j], parameters[iteration][1]):
+            handler(parameters[iteration][1], path)
         else:
             parameters = input(
-                f"Invalid date '{parameters[i+1]}'. " f"Please enter a valid command: "
+                f"Invalid date '{parameters[iteration][1]}'. "
+                f"Please enter a valid command: "
             ).split(" ")
-            i = 0
-            path = ""
+            parameters, args = getopt.getopt(parameters, ":e:a:")
+            path = constants.WEATHER_FILES_DIR
+            if args:
+                path, parameters = args[0], getopt.getopt(args[1:], ":e:a:")[0]
+            iteration = 0
             continue
-        i += 2
+        iteration += 1
         print()
 
 
