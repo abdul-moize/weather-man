6e476df2e7d6899e466338d3d6538a9e7b8334b8
Abdul-Moize <abdul.moize@arbisoft.com>
2021-08-20 14:22:44 +0500

    change return type to None for failure

--- modules/utils.py
+++ modules/utils.py
@@ -7,7 +7,7 @@ import glob
 def pattern_search(pattern, path):
     """
     This function returns all the files in the given path that match the pattern given
-     Args:
+    Args:
         pattern(str): a string which contains any pattern e.g '2006', '2007', '2005/6', etc
         path(str): a string which contains path to a directory containing weather files
     Returns:
@@ -23,12 +23,12 @@ def read_data(year, path):
         year(str): 4digit string like '2002', '2003', '2004', etc
         path(str): like any path string 'weatherfile/', 'path/to/files', etc
     Returns:
-        generator object containing row of a file or -1 if no file exists for a given year
+        generator object containing row of a file or None if no file exists for a given year
     """
     files = pattern_search(year, path)
     if not files:
         print(f"We don't have information regarding the year {year} in the given path")
-        yield -1
+        yield None
     for i in files:
         with open(i, "r") as file:
             # skip first line as it contains field names
